481881fc6a5a26d920ffbed3129ec11c54cd8ecc
damian3111 <damiankwasny311@gmail.com>
2023-08-16 15:19:17 +0200

    jenkins

---
+++ src/test/java/com/damian3111/demo/SampleTests.java
@@ -0,0 +1,14 @@
+package com.damian3111.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SampleTests {
+
+    @Test
+    public void dummy() {
+
+        assertThat(true).isEqualTo(true);
+    }
+}
